870a9b662be261b1675823aee08d839b7af2178d
Parvsub <Praveenboda421@gmail.com>
2023-02-11 17:06:27 +0530

    Added: Welcome meassage is printed

---
+++ WelcomeMessage.java
@@ -0,0 +1,7 @@
+package addressBookMangementSystem;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
