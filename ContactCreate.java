1f48fb3fd6a0911525b8ba953002be897c713b32
Parvsub <Praveenboda421@gmail.com>
2023-02-11 21:11:21 +0530

    Added: Contacts are added

---
+++ ContactCreate.java
@@ -0,0 +1,89 @@
+package addressBookMangementSystem;
+public class ContactCreate {
+    private String firstName,lastName,address,city,state,zip,phoneNumber,email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public ContactCreate(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber,String email ){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+    }
+    public ContactCreate(){
+        super();
+    }
+    @Override
+    public String toString() {
+        return "Employee [name=" + this.firstName +" "+ this.lastName + ", Address=" + this.address + ", City="
+                + this.city + ", state=" + this.state + ", zip=" + this.zip
+                + ", phone=" + this.phoneNumber + "]";
+    }
+}
+
